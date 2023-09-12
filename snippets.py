d0c202de9c541c9fad8ec338b89cb693adc08402
Sandra Chotia <sandra.chotia@gmail.com>
2023-09-12 15:24:54 -0700

    add working w3d1 afterclass code/

--- snippets.py
+++ snippets.py
@@ -8,27 +8,28 @@ def snippet_1():
     u = 5
     v = 2
 
-    if u * v = 10:
+    if u * v == 10:
         print(f"The product of u ({u}) and v ({v}) is 10")
     else:
         print(f"The product of u ({u}) and v ({v}) is not 10")
 
+#snippet_1()
 
 def snippet_2():
     x = 15
     y = 25
-
     z = 30
 
     if z < x:
         print("z is less than x")
 
-    elif z > x and z < y
+    elif z > x and z < y:
         print("z is between x and y")
 
-    else
+    else:
         print("z is greater than y")
 
+#snippet_2()
 
 def snippet_3():
     # TODO: Modify the comparison operator below so the `assert` statement passes
@@ -36,20 +37,24 @@ def snippet_3():
 
     a = 1
     b = 1
-    c = (a > b)
+    c = (a == b)
 
-    print(f"The value of c ({c}) is True since a ({a}) is greater than b ({b}).")
+    print(f"The value of c ({c}) is True since a ({a}) is equal to b ({b}).")
     assert(c == True)  # <-- DO NOT EDIT THIS LINE
 
+#snippet_3()
 
 def snippet_4():
     # TODO: Modify exactly one boolean operator in the assignment of d, so that d evaluates to False
 
-    d = (5 < 7) or not (8 < 20)
-
+    d = (5 > 7) and not (8 < 20)
     # TODO: Explain how d is set to False in a comment
-    assert(d == False)  # <-- DO NOT EDIT THIS LINE
+    #by changing the 5 < 7 to 5 > 7 the first comparison is false, the second won't even be looked at because at the 'and' the condition false was determined
 
+    assert(d == False)  # <-- DO NOT EDIT THIS LINE
+    #by using and not the not flips the value of the true statement 8 < 20 to a false value
+
+#snippet_4()
 
 def snippet_5():
     # TODO: Modify the comparison operator so o evalutes to true
@@ -58,7 +63,9 @@ def snippet_5():
     m = "GOAT"
     n = "goat"
 
-    o = (m == n)
+    o = (m != n)
 
-    print (f"The value of o ({o}) is False since Python is case-sensitive.")
+    print (f"The value of o ({o}) is True since Python is case-sensitive.")
     assert(o == True)  # <-- DO NOT EDIT THIS LINE
+
+#snippet_5()
--- stocks.py
+++ stocks.py
@@ -1,28 +1,41 @@
 
+#S-here I'm using a dictionary and key : value pairs to map the stock name to price
+stocks = {
+'amazon' : 3000,
+'apple' : 100,
+'fb' : 250,
+'google' : 1400,
+'msft' : 200
+}
+
 def stock_purchases():
-    amazon = 3000
-    apple = 100
-    fb = 250
-    google = 1400
-    msft = 200
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
     # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
-
+    user_input1 = input("what is your name?")
+    name = user_input1
     # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
+    user_input2 = int(input("How much would you like to invest? $"))
     # and save it into a variable
+    dollars = user_input2
     # NOTE: When you use the `input` function to get user input, what do numbers get saved as?
-
     # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
     # NOTE: Take a look at how this input string prints out
-    # stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
-    
+    stock_name = input(f"\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
+    stock_price = stocks.get(stock_name.lower())  
     # Now that you have all of the client info, you can check how much they can purchase of the stock
     # they're interested in.
 
     # 1.4 TODO: Use `if/elif/else` conditional logic to determine how much stock the client can buy,
     # and save it in a variable
-
+    if dollars > stock_price:
+        share = int(dollars / stock_price)
+    elif dollars < stock_price:
+        print("You can't buy one full share.")
     # 1.5 TODO: Once you've calculated the number of stocks that can be purchased,
     # Use an f-string to print the result for the client, ala:
     # Alex has $5000 to invest and can buy 50 shares of Apple at the current price of $100.
+    
+    print(f'{name} has ${dollars} to invest and can buy {share} shares of {stock_name} at the current price of ${stock_price}.')
+
+#stock_purchases()
